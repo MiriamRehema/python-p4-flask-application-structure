bf304ada12fbc3925245c4029dd7a6c4c28bc555
MiriamRehema <rehemamiriam30@gmail.com>
2023-09-19 09:14:33 +0300

    building-flask

--- server/app.py
+++ server/app.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return '<h1>Welcome to my app!</h1>'
-@app.route('/string:<username>')
+
+@app.route('/<string:username>')
 def user(username):
     return f'<h1>Profile for {username}</h1>'
 
